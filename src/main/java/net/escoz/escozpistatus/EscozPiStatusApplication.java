7b85049db5fb8848885e83b167f8d27bc642827f
Escoz <kyrovilches@gmail.com>
2024-09-21 11:07:36 +0200

    Commit inicial

---
+++ src/main/java/net/escoz/escozpistatus/EscozPiStatusApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.escozpistatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscozPiStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscozPiStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/escozpistatus/EscozPiStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.escozpistatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscozPiStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
