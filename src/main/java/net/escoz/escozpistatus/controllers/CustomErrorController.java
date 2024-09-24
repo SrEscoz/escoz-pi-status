7fd98bcdefd5973072c3ea46abdbec1f30fe25d8
Miguel Escoz <kyrovilches@gmail.com>
2024-09-24 23:25:16 +0200

    Corregido error en el css de error.html

--- src/main/java/net/escoz/escozpistatus/controllers/CustomErrorController.java
+++ src/main/java/net/escoz/escozpistatus/controllers/CustomErrorController.java
@@ -2,29 +2,18 @@ package net.escoz.escozpistatus.controllers;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.boot.web.error.ErrorAttributeOptions;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.WebRequest;
-
-import java.util.Map;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
 	private static final String ERROR_PATH = "/error";
 
-	private final ErrorAttributes errorAttributes;
-
-	public CustomErrorController(ErrorAttributes errorAttributes) {
-		this.errorAttributes = errorAttributes;
-	}
-
 	@RequestMapping("/error")
-	public String handleError(HttpServletRequest request, WebRequest webRequest, Model model) {
+	public String handleError(HttpServletRequest request, Model model) {
 		// Obtener el código de error
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
