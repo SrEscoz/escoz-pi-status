6cbcdff47ff84a3169fd5abc9a792c30c504ea40
Miguel Escoz <kyrovilches@gmail.com>
2024-09-24 22:49:00 +0200

    Actualización del main.html

--- src/main/java/net/escoz/escozpistatus/controllers/WebController.java
+++ src/main/java/net/escoz/escozpistatus/controllers/WebController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import net.escoz.escozpistatus.entities.PiService;
 import net.escoz.escozpistatus.entities.mappers.PiServiceMapper;
 import net.escoz.escozpistatus.services.PiServiceService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +18,15 @@ public class WebController {
 	private PiServiceService piServiceService;
 
 	@RequestMapping("/")
-	public String index(Model model) {
+	public String index(Model model, Authentication auth) {
 		List<PiService> piServices = piServiceService.getAllServices();
 		long inactiveCount = piServices.stream()
 				.filter(service -> !service.getActive()).count();
+		boolean isLogged = auth != null && auth.isAuthenticated();
 
 		model.addAttribute("services", PiServiceMapper.INSTANCE.toDTOList(piServices));
 		model.addAttribute("inactiveCount", inactiveCount);
+		model.addAttribute("isLogged", isLogged);
 
 		return "main";
 	}
